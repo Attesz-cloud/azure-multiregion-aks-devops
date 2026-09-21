30eb132325b9352b717afa6b20c88820056c09a9
Attesz-cloud <gatyamtya45@gmail.com>
2026-09-21 13:49:52 +0500

    Fix: correct local variable formatting and peering network id argument

--- main.tf
+++ main.tf
@@ -21,38 +21,38 @@ locals {
   }
 }
 
-
+# 1. RÉGIÓ: West Europe (Hub, Spoke, AKS)
 module "region_primary" {
   source          = "./modules/azure_region"
   region_location = "West Europe"
   region_short    = "westeurope"
-  project_name    = locals.project_name
-  environment     = locals.environment
+  project_name    = local.project_name
+  environment     = local.environment
   hub_cidr        = "10.100.0.0/16"
   spoke_cidr      = "10.101.0.0/16"
   aks_subnet_cidr = "10.101.1.0/24"
-  tags            = locals.tags
+  tags            = local.tags
 }
 
-
+# 2. RÉGIÓ: North Europe (Hub, Spoke, AKS)
 module "region_secondary" {
   source          = "./modules/azure_region"
   region_location = "North Europe"
   region_short    = "northeurope"
-  project_name    = locals.project_name
-  environment     = locals.environment
+  project_name    = local.project_name
+  environment     = local.environment
   hub_cidr        = "10.200.0.0/16"
   spoke_cidr      = "10.201.0.0/16"
   aks_subnet_cidr = "10.201.1.0/24"
-  tags            = locals.tags
+  tags            = local.tags
 }
 
-
+# GLOBAL VNET PEERING (A két régió Hub hálózatának összekötése)
 resource "azurerm_virtual_network_peering" "global_primary_to_secondary" {
   name                         = "peer-global-westeurope-to-northeurope"
   resource_group_name          = module.region_primary.resource_group_name
   virtual_network_name         = module.region_primary.hub_vnet_name
-  remote_network_id            = module.region_secondary.hub_vnet_id
+  remote_virtual_network_id    = module.region_secondary.hub_vnet_id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
 }
@@ -61,7 +61,7 @@ resource "azurerm_virtual_network_peering" "global_secondary_to_primary" {
   name                         = "peer-global-northeurope-to-westeurope"
   resource_group_name          = module.region_secondary.resource_group_name
   virtual_network_name         = module.region_secondary.hub_vnet_name
-  remote_network_id            = module.region_primary.hub_vnet_id
+  remote_virtual_network_id    = module.region_primary.hub_vnet_id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
 }
